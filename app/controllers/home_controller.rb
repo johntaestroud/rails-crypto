e1b68c9048cf72d40904946c9d86780af1ea423a
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-21 14:12:09 -0400

    Implemented API

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @hello = "Hello there!"
+    #require 'net/http'
+    #require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
   end
 
   def about
